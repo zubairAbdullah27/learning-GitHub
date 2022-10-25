2ed6074ad7fd6dbf4df181df967f8c5d6854ec2f
zubairAbdullah27 <zubaairkhhan@gmail.com>
2022-10-25 14:52:51 +0500

    Initial Commit

---
+++ Learning-Github/Learning_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GithubApp.swift
+//  Learning-Github
+//
+//  Created by Mac on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
